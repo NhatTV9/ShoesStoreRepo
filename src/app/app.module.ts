49bcf32785292b5e46c2cb2dd1165fc24c02bec3
Hoang Thanh Nga <nga12012000@gmail.com>
2022-05-31 16:52:30 +0700

    implement auth login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -52,6 +52,7 @@ import { DealWeekComponent } from './components/deal-week/deal-week.component';
     MatInputModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
