b64ad06dfde26bf091bf7a43beebeeb7f7041c69
Hoang Thanh Nga <nga12012000@gmail.com>
2022-06-01 08:49:07 +0700

    implement login 2

---
+++ src/app/servicesdata/auth.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
+export interface User {
+  _id: string;
+  name: string;
+  email: string;
+  lastName: string;
+  location: string;
+  __v: number;
+}
+
+export interface UserRespone {
+  user: User;
+  token: string;
+  location: string;
+}
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private baseUrl = 'https://jobify-prod.herokuapp.com/api/v1';
+  public user = null;
+  private savedToken = 'token';
+  constructor(private http: HttpClient) {}
+  login(user, stayLogIn) {
+    return this.http.post(`${this.baseUrl}/auth/login`, user).pipe(
+      tap((res: UserRespone) => {
+        this.user = res.user;
+        if (stayLogIn) {
+          localStorage.setItem(this.savedToken, res.token);
+        }
+      })
+    );
+  }
+}
