2ec5b81a0c2b2ce46f126b0e04a6b7dc9fa27976
Tran Van Nhat <nhattvdx@gmail.com>
2022-06-02 16:05:42 +0700

    logic search

--- src/app/components/shopping-cart/shopping-cart.component.ts
+++ src/app/components/shopping-cart/shopping-cart.component.ts
@@ -7,26 +7,15 @@ import { CartService } from '../../servicesdata/cart.service';
   styleUrls: ['./shopping-cart.component.scss'],
 })
 export class ShoppingCartComponent implements OnInit {
-
   deliveryfee = 50;
   constructor(public cartService: CartService) {}
 
   ngOnInit(): void {}
   getTotalPrice() {
     let total = 0;
-  public total = 0;
-  constructor(public cartService: CartService) {
     for (let i of this.cartService.getCart()) {
       total += i.quantity * Number.parseFloat(i.item.price);
     }
     return total;
-  }
-
-  ngOnInit(): void {}
-  ngOnChanges() {
-    for (let i of this.cartService.getCart()) {
-      console.log(typeof i.item.price);
-      this.total += i.quantity * Number.parseFloat(i.item.price);
-    }
   }
 }
