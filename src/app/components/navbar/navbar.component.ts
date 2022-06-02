3ec96b7299bf570653163199c832c79761aa3022
Tran Van Nhat <nhattvdx@gmail.com>
2022-06-02 15:56:10 +0700

    logic search 1

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { NavigationEnd, Router } from '@angular/router';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  map,
+  switchMap,
+  tap,
+} from 'rxjs/operators';
+import { ProductsService } from 'src/app/servicesdata/products.service';
 import { AuthService } from '../../servicesdata/auth.service';
 import { CartService } from '../../servicesdata/cart.service';
 
@@ -9,13 +20,40 @@ import { CartService } from '../../servicesdata/cart.service';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-  public showSearch = false;
+  public showDropdown = false;
+  public productSearch = [];
   constructor(
     private router: Router,
+    private http: HttpClient,
     public authService: AuthService,
-    public cartService: CartService
+    public cartService: CartService,
+    private productService: ProductsService
   ) {}
-  ngOnInit(): void {}
+  search = new FormControl('');
+  ngOnInit(): void {
+    this.search.valueChanges
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged(),
+        map((value: string) => {
+          return value.trim();
+        }),
+        tap((v: string) => {
+          if (v === '') {
+            this.showDropdown = false;
+          }
+        }),
+        filter((value: string) => value !== '')
+      )
+      .subscribe((v) => {
+        console.log();
+
+        this.productSearch = this.productService.productList.filter((p) => {
+          return p.title.includes(v);
+        });
+        console.log(this.productSearch);
+      });
+  }
   onLogout() {
     this.authService.logout();
     this.router.navigateByUrl('/login');
